6eb5d261489ff5203b5f0c6467f1348a3fafb1cb
zhitkov-hfd <raxil@internet.ru>
2026-07-01 18:22:03 +0300

    Create mission.py

---
+++ core/mission.py
@@ -0,0 +1,8 @@
+def get_mission():
+    return {
+        "workout": "50 отжиманий",
+        "steps": "10000 шагов",
+        "water": "2 литра воды",
+        "reading": "30 минут чтения",
+        "sleep": "до 23:00"
+    }
